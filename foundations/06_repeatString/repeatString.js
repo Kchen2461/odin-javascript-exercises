d0c3dd641a2c23d470ebaa31becc6ceefd4402ee
Kevin Chen <kchen2461@gmail.com>
2026-03-27 22:47:09 +0000

    Feat: Complete 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+function repeat(string, num) {
+    let newStr = "";
+    for (let i = 0; i < num; i++) {
+        newStr += string;
+    }
 
+    return newStr;
+}
+
+const repeatString = function(string, num) {
+    return (num < 0) ? `ERROR` : repeat(string, num);
 };
 
 // Do not edit below this line
