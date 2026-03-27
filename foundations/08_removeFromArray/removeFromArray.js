527d614ffff195d0316126ca09b627c2d65c98c8
Kevin Chen <kchen2461@gmail.com>
2026-03-27 23:25:29 +0000

    Feat: Complete 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    let args = Array.from(arguments);
+    let newArr = [];
+
+    for (item of arr) {
+        if (args.includes(item)) {
+            continue;
+        }
+
+        newArr.push(item);
+    }
+
+    return newArr;
 };
 
 // Do not edit below this line
