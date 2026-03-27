b69ee004c293e8045b9e5982c70a059bfbc68bf1
Kevin Chen <kchen2461@gmail.com>
2026-03-27 23:41:12 +0000

    Feat: Complete 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let nums = [startNum, endNum].sort();
 
+    // If not a number or not a positive number, return ERROR
+    if (!Number.isInteger(nums[0]) || nums[0] < 0 || !Number.isInteger(nums[1]) || nums[1] < 0) {
+        return "ERROR";
+    }
+
+    // Math way. Formula for sum between two integers (including start and end integers)
+    //return (nums[1] - nums[0] + 1) * (nums[0] + nums[1]) / 2;
+
+    let total = 0;
+
+    for (let i = nums[0]; i <= nums[1]; i++) {
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
