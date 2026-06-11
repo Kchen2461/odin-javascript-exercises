ffb5df135adcc05e94d730d9ede7c290c8e7ec69
Kevin Chen <kchen2461@gmail.com>
2026-06-11 19:16:24 +0000

    Feat: Complete js/foundations/13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    // The regex for detecting characters can be improved
+    let regex = /[(a-z)(A-Z)(0-9)]/;
+    let letters = str.split("").reduce((letters, currLetter) => {
+        if (regex.test(currLetter)) {
+            letters += currLetter.toLowerCase();
+        }
+
+        return letters;
+    }, "");
+
+    // Compare the reverse the first half of the string and the second half
+    return letters.slice(0, (letters.length + 1) / 2).split("").reverse().join("") === letters.slice(letters.length / 2);
 };
 
 // Do not edit below this line
