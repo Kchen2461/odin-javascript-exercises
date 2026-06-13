081616f0d79a6f6774019a0de6111e33b85a9258
Kevin Chen <kchen2461@gmail.com>
2026-06-13 06:15:00 +0000

    Feat: Complete 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    if (people.length == 0) {
+        return 0;
+    }
+
+    let oldest_person = people[0];
+    
+    people.reduce((oldest_age, person) => {
+
+        let age = 0;
+
+        // If the person has not died yet, use today's date to calculate their age
+        if (!("yearOfDeath" in person)) {
+            let curr_date = new Date();
+            age = curr_date.getFullYear() - person["yearOfBirth"];
+        } else {
+            age = person["yearOfDeath"] - person["yearOfBirth"];
+        }
+
+        if (age > oldest_age) {
+            oldest_person = person;
+            return age;
+        }
+    }, 0);
+
+    return oldest_person;
 };
 
 // Do not edit below this line
